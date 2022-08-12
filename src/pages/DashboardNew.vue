f886b85cfcbc91c43be37d7de086da4f41d9c8d3
Adeelshaikh01 <adeelshaikh654@gmail.com>
2022-08-12 20:54:45 +0500

    first commit

---
+++ src/pages/DashboardNew.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+             <div class="col-12  position-sticky fixed-top ">
+                <router-view :key="$route.fullPath" />
+            </div>
+
+            <div class="col-12">
+                <MonitorListNew :scrollbar="true" />
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+
+import MonitorListNew from "../components/MonitorListNew.vue";
+
+export default {
+    components: {
+        MonitorListNew,
+    },
+    data() {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/vars.scss";
+.container-fluid {
+    width: 98%;
+}
+</style>
